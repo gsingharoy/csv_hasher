b069995f81184ad00f20c07812c1deb1be557a34
Gaurav SinghaRoy <neogauravsvnit@gmail.com>
2015-05-25 08:26:33 +0200

    added another parameter 'options'

--- lib/csv_hasher.rb
+++ lib/csv_hasher.rb
@@ -2,14 +2,18 @@ require 'csv'
 
 class CSVHasher
 
-  def self.hashify(path_to_csv)
+  # This method will return arrays of hashes for the CSV rows
+  # The keys for the hashes can also be passed in the order it should appear
+  def self.hashify(path_to_csv, options = {})
     csv_arrs = CSV.read(path_to_csv)
-    keys = col_keys(csv_arrs[0])
-    csv_arrs[1..-1].map {|a| Hash[ keys.zip(a) ] }
+    keys = options[:keys] || col_keys(csv_arrs[0])
+    start_index = options[:keys].nil? || options[:include_headers] ? 1 : 0 
+    csv_arrs[start_index..-1].map {|a| Hash[keys.zip(a)] }
   end
 
   private 
 
+  # this method converts the header of the CSV into an array of sym keys 
   def self.col_keys(keys)
     keys.map{ |k| k.gsub('  ',' ').gsub(' ','_').downcase.to_sym }
   end
