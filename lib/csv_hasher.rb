b82ffc3f12511bfb0ab1abc310eb85a0010ccc97
gsingharoy <neogauravsvnit@gmail.com>
2015-05-27 17:48:10 +0200

    added another option original_col_as_keys

--- lib/csv_hasher.rb
+++ lib/csv_hasher.rb
@@ -7,21 +7,31 @@ class CSVHasher
   # Example:
   #   >> CSVHasher.hashify('path/to/csv/file')
   #   => [{:col_1=> '..', :col_2=> '..'},...]
-  #   
+  #
   #
   # Arguments:
   #   path_to_csv: (String)
   #   options: (Hash)
-  def self.hashify(path_to_csv, options = {}) 
+  #            original_col_as_keys: (Boolean)
+  #                                   if original_col_as_keys is true then keys are exactly the col headers
+  #                                   if original_col_as_keys is false then keys are spidercase symbols from col headers
+  #            keys: (Array) Custom keys for the returned hashes
+  #
+  #
+  def self.hashify(path_to_csv, options = { original_col_as_keys: false })
     csv_arrs = CSV.read(path_to_csv)
-    keys = options[:keys] || col_keys(csv_arrs[0])
-    start_index = options[:keys].nil? || options[:include_headers] ? 1 : 0 
+    if options[:original_col_as_keys]
+      keys = csv_arrs[0]
+    else
+      keys = options[:keys] || col_keys(csv_arrs[0])
+    end
+    start_index = options[:keys].nil? || options[:include_headers] ? 1 : 0
     csv_arrs[start_index..-1].map {|a| Hash[keys.zip(a)] }
   end
 
-  private 
+  private
 
-  # this method converts the header of the CSV into an array of sym keys 
+  # this method converts the header of the CSV into an array of sym keys
   def self.col_keys(keys)
     keys.map{ |k| k.gsub('  ',' ').gsub(' ','_').downcase.to_sym }
   end
--- spec/lib/csv_hasher_spec.rb
+++ spec/lib/csv_hasher_spec.rb
@@ -1,36 +1,36 @@
 require 'spec_helper'
 describe CSVHasher do
-  describe '::hashify' do 
-    context 'when no file exists' do 
-      it 'raises error Errno::ENOENT' do 
+  describe '::hashify' do
+    context 'when no file exists' do
+      it 'raises error Errno::ENOENT' do
         expect{
           CSVHasher.hashify('/incorrect/path/to/csv')
         }.to raise_error Errno::ENOENT
       end
     end
-    context 'when file exists' do 
+    context 'when file exists' do
       let(:sample1_path) { File.join(
-                              File.expand_path(File.dirname(__FILE__)), 
-                              '..', 
-                              'fixtures', 
+                              File.expand_path(File.dirname(__FILE__)),
+                              '..',
+                              'fixtures',
                               'sample1.csv') }
-      context 'sample1.csv file' do 
-        before do 
-          @header_keys = [:col_1, :col_2, :col_3] 
+      context 'sample1.csv file' do
+        before do
+          @header_keys = [:col_1, :col_2, :col_3]
           @total_rows = 3
         end
-        it 'does not raise any error' do 
+        it 'does not raise any error' do
           expect{
             CSVHasher.hashify(sample1_path)
           }.to_not raise_error
         end
-        it 'returns the headers as keys' do 
+        it 'returns the headers as keys' do
           returned_arrs = CSVHasher.hashify(sample1_path)
           returned_arrs.each do |ra|
             expect(ra.keys).to eq @header_keys
           end
         end
-        it "returns #{@total_rows} hashes" do 
+        it "returns #{@total_rows} hashes" do
           expect(CSVHasher.hashify(sample1_path).count).to eq @total_rows
         end
         [1, 2, 3].each do |col_no|
@@ -43,8 +43,8 @@ describe CSVHasher do
             end
           end
         end
-        context 'when keys are passed in options parameter' do 
-          it 'returns arrays of hashes with the keys passed' do 
+        context 'when keys are passed in options parameter' do
+          it 'returns arrays of hashes with the keys passed' do
             msg_keys = ['col 1', 'col 2', 'col 3']
             returned_arrs = CSVHasher.hashify(sample1_path, { keys: msg_keys })
               returned_arrs.each do |ra|
@@ -52,34 +52,43 @@ describe CSVHasher do
             end
           end
         end
+        context 'when original_col_as_keys is true' do
+          it 'returns arrays of hashes with original headers as keys' do
+            col_arr = ['col 1', 'col 2', 'col 3']
+            returned_arrs = CSVHasher.hashify(sample1_path, { original_col_as_keys: true })
+            returned_arrs.each do |ra|
+              expect(ra.keys).to eq col_arr
+            end
+          end
+        end
       end
     end
   end
 
-  describe '::col_keys' do 
-    context 'when all keys are lower case' do 
-      it 'returns the keys as it is' do 
+  describe '::col_keys' do
+    context 'when all keys are lower case' do
+      it 'returns the keys as it is' do
         msg = ['foo', 'bar', 'lorem']
         expected_response = [:foo, :bar, :lorem]
         expect(CSVHasher.send(:col_keys, msg)).to eq expected_response
       end
     end
-    context 'when keys are capitalized' do 
-      it 'returns the keys in lower case' do 
+    context 'when keys are capitalized' do
+      it 'returns the keys in lower case' do
         msg = ['Foo', 'Bar', 'LOrem']
         expected_response = [:foo, :bar, :lorem]
         expect(CSVHasher.send(:col_keys, msg)).to eq expected_response
       end
     end
-    context 'when keys have spaces' do 
-      it 'returns the keys in spider case' do 
+    context 'when keys have spaces' do
+      it 'returns the keys in spider case' do
         msg = ['Foo Bar', 'Lorem  IPsum']
         expected_response = [:foo_bar, :lorem_ipsum]
         expect(CSVHasher.send(:col_keys, msg)).to eq expected_response
       end
     end
-    context 'when keys have numbers' do 
-      it 'returns numbers in the keys' do 
+    context 'when keys have numbers' do
+      it 'returns numbers in the keys' do
         msg = ['Foo Bar1', 'Lorem  IPsum2 4']
         expected_response = [:foo_bar1, :lorem_ipsum2_4]
         expect(CSVHasher.send(:col_keys, msg)).to eq expected_response
